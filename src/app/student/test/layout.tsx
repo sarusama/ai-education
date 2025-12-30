8c05d0309b27eca9489b0f8378df043f6083e000
sarusama <ld839183343@163.com>
2025-12-30 14:46:55 +0800

    feat: 优化打包

--- src/app/student/test/layout.tsx
+++ src/app/student/test/layout.tsx
@@ -1,16 +1,8 @@
-// Generate static params for static export
-export async function generateStaticParams({ classId, id }: { classId: string; id: string }) {
-  return {
-    classId,
-    id,
-  };
-}
-
 export default function StudentIdLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return children;
 }
 
